5d05e2ffcc96d3fe323477b698442edeee671b27
SuperXavier42 <superxavier100@gmail.com>
2026-02-10 19:45:29 +0000

    second

--- src/main/java/Window.java
+++ src/main/java/Window.java
@@ -1,15 +1,13 @@
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
+
 
 /**
  *
  * @author super
  */
-public class ProjectManager {
-
-    public static void main(String[] args) {
-        System.out.println("Hello World!");
-    }
-}
+public class Window {
     
+}
